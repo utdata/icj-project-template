848db731988ffd2d79696df9d8b84f10a62ba629
Christian McDonald <christian.mcdonald@utexas.edu>
2018-10-29 21:17:59 -0500

    getting data to work in some way

--- tasks/nunjucks.js
+++ tasks/nunjucks.js
@@ -3,7 +3,7 @@ const nunjucksRender = require('gulp-nunjucks-render');
 const journalize = require('journalize');
 const data = require('gulp-data');
 const browserSync = require('browser-sync').create();
-// const config = require('../project.config');
+const config = require('../project.config');
 
 module.exports = () => {
   // helper function for nunjucks render
@@ -17,12 +17,11 @@ module.exports = () => {
   const manageEnv = function(env) {
     // loop over config vars to add to nunjucks global env
     // which can be added to project.config.json
-
-    // for (var k in config) {
-    //   if(config.hasOwnProperty(k)) {
-    //     env.addGlobal(k, config[k]);
-    //   }
-    // }
+    for (var k in config) {
+      if(config.hasOwnProperty(k)) {
+        env.addGlobal(k, config[k]);
+      }
+    }
 
     // set up journalize
     for (let key in journalize) {
