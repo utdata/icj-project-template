0b4ef55171ccfae73fa1a93bcc5b31379a5acef2
Christian McDonald <christian.mcdonald@utexas.edu>
2019-03-09 08:39:41 -0600

    fixing browserreload issue

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,18 +1,23 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const reload = browserSync.reload;
- 
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
+
 module.exports = () => {
   browserSync({
     notify: false,
     server: 'docs',
     port: 3000
-  });
-  gulp.watch(['src/njk/**/*.html','src/njk/**/*.njk','src/njk/**/*.json'], gulp.series('nunjucks', reload));
+  }),
+  gulp.watch(['src/njk/**/*.html','src/njk/**/*.njk','src/njk/**/*.json'], gulp.series('nunjucks', browserSyncReload));
   gulp.watch(
     ['node_modules/bootstrap/scss/bootstrap.scss','src/scss/**/*.scss'],
-    gulp.series('styles', reload)
+    gulp.series('styles', browserSyncReload)
     );
-  gulp.watch(['src/js/**/*.js'], gulp.series('lint', 'scripts', reload));
-  gulp.watch(['src/assets/img/**/*'], gulp.series('images', reload));
+  gulp.watch(['src/js/**/*.js'], gulp.series('lint', 'scripts', browserSyncReload));
+  gulp.watch(['src/assets/img/**/*'], gulp.series('images', browserSyncReload));
 };
